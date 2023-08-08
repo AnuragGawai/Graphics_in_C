7e331881e526f81bf7b1d8b50f314460bed15aa9
Anurag Eknath Gawai <gawaianurag4@gmail.com>
2023-08-08 09:06:52 +0530

    Trancelation

---
+++ Basic_Graphics/TRANCELA.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
+#include<stdlib.h>
+
+void main()
+     {
+     int gd=DETECT , gm;
+     int x1, x2, y1, y2, x,y;
+     printf("Enter line co ordinate \n");
+     scanf("%d%d%d%d",&x1,&x2,&y1,&y2);
+     initgraph(&gd, &gm,"c://turboc3//bgi");
+     line(x1,y1,x2,y2);
+     printf("Enter Trancelation Factor\n");
+     scanf("%d%d",&x,&y);
+     x1=(x1+x);
+     y1=(y1+y);
+     x2=(x2+x);
+     y2=(y2+y);
+      printf("print line after trancelation");
+	line(x1,y1,x2,y2);
+
+
+ getch();
+ closegraph();
+
+ }
+
